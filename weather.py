b9a81cc76a1fb903238302011f6336ed69770b1c
Najwa Husin <wnnajwaathirah@gmail.com>
2024-01-10 17:54:14 +0800

    done3

--- weather.py
+++ weather.py
@@ -18,6 +18,12 @@ if __name__ == "__main__":
 
     city = input("\nPlease enter a city name: ")
 
+    # Check for empty strings or string with only spaces
+
+    if not bool(city.strip()):
+        city = "London"
+        
+
     weather_data = get_current_weather(city)
 
     print("\n")
